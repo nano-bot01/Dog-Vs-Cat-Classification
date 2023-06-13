88b5ca999c29e16e79a7302de8835699618da438
Ankit Nainwal <78251168+nano-bot01@users.noreply.github.com>
2023-06-13 14:09:48 +0530

    Create data.bash

---
+++ data.bash
@@ -0,0 +1,19 @@
+ dataset/
+├── train/
+│   ├── dog/
+│   │   ├── dog_image1.jpg
+│   │   ├── dog_image2.jpg
+│   │   └── ...
+│   └── cat/
+│       ├── cat_image1.jpg
+│       ├── cat_image2.jpg
+│       └── ...
+└── test/
+    ├── dog/
+    │   ├── dog_image1.jpg
+    │   ├── dog_image2.jpg
+    │   └── ...
+    └── cat/
+        ├── cat_image1.jpg
+        ├── cat_image2.jpg
+        └── ...
